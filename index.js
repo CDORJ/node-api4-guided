2f3628f9f28098af2257f86ed54afd32511e8a73
ChadDiaz <chadcdiaz@gmail.com>
2021-03-17 23:05:17 -0400

    created motd

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const server = require("./api/server.js");
 
 const port = process.env.PORT;
-console.log(process.env)
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
