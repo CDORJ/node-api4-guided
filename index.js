fc4f1376beaf21274485d007498e139c67495499
ChadDiaz <chadcdiaz@gmail.com>
2021-03-17 22:32:03 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT;
+console.log(process.env)
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
